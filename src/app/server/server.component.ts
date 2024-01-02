c5366958a0f4fe166cbb0274eada572b71c8ba43
Ruthless <vdev13579@gmail.com>
2024-01-02 14:55:13 +0530

    adding assignment 3

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,13 @@ import {Component} from "@angular/core";
   templateUrl: './server.component.html',
 })
 export class ServerComponent {
-  static NoOfAlerts: number = 0;
+  static NoOfAlerts = 0;
+  displayDetails = false;
+  logs: string[] = [];
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    const timestamp = new Date();
+    this.logs.push(`Clicked at ${timestamp}`);
+  }
 }
